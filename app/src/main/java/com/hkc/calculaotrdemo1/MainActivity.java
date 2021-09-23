2686942839ca2df32b8598206f6be81af599aff3
terryhung009 <khc741213@gmail.com>
2021-09-23 16:27:46 +0800

    調整layout

--- app/src/main/java/com/hkc/calculaotrdemo1/MainActivity.java
+++ app/src/main/java/com/hkc/calculaotrdemo1/MainActivity.java
@@ -1,14 +1,21 @@
 package com.hkc.calculaotrdemo1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import com.hkc.calculaotrdemo1.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+
+
     }
 }
